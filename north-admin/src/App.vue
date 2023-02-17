9a90b930af5889cb0cf57f84e8ce730c7d3f3d1d
north <2445951561@qq.com>
2023-02-17 14:44:57 +0800

    Install Naive-UI TailwindCss axios pinia

--- north-admin/src/App.vue
+++ north-admin/src/App.vue
@@ -1,9 +1,44 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import { Airplane24Filled, AnimalRabbit24Regular } from '@vicons/fluent'
 </script>
 
 <template>
   <div>
+    <div class="truncate w-60 h-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+      repellendus
+      odit
+      pariatur dolorum molestias possimus. Quam excepturi, quo molestiae est culpa sapiente accusamus perspiciatis maiores
+      magnam? Aut nesciunt ipsam commodi!</div>
+    <n-space>
+      <n-button>Default</n-button>
+      <n-button type="tertiary">
+        Tertiary
+      </n-button>
+      <n-button type="primary">
+        Primary
+      </n-button>
+      <n-button type="info">
+        Info
+      </n-button>
+      <n-button type="success">
+        Success
+      </n-button>
+      <n-button type="warning">
+        Warning
+      </n-button>
+      <n-button type="error">
+        Error
+      </n-button>
+    </n-space>
+    <n-space>
+      <n-icon size="40" color="#0e7a0d">
+        <Airplane24Filled />
+      </n-icon>
+      <n-icon size="30" color="red">
+        <AnimalRabbit24Regular />
+      </n-icon>
+    </n-space>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -21,9 +56,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
--- north-admin/src/main.ts
+++ north-admin/src/main.ts
@@ -1,5 +1,15 @@
+/*
+ * @Author: north 2445951561@qq.com
+ * @Date: 2023-02-17 09:53:46
+ * @LastEditors: north 2445951561@qq.com
+ * @LastEditTime: 2023-02-17 10:16:32
+ * @FilePath: \north\north-admin\src\main.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import naive from "naive-ui";
+import './styles/tailwind.css' // 引入tailwind.css
 
-createApp(App).mount('#app')
+createApp(App).use(naive).mount('#app')
