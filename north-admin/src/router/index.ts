e311d9682c116a886cb09fe71d11e055808bb382
north <2445951561@qq.com>
2023-04-14 11:08:19 +0800

    新增404页面

--- north-admin/src/router/index.ts
+++ north-admin/src/router/index.ts
@@ -2,7 +2,7 @@
  * @Author: north 2445951561@qq.com
  * @Date: 2023-03-28 14:23:09
  * @LastEditors: north 2445951561@qq.com
- * @LastEditTime: 2023-04-04 10:04:32
+ * @LastEditTime: 2023-04-14 10:26:12
  * @FilePath: \north\north-admin\src\router\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,6 +16,18 @@ const routes: Array<RouteRecordRaw> = [
     meta: {
       title: '登陆'
     }
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: () => import('../views/error/404.vue'),
+    meta: {
+      title: '404，页面无法访问'
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/404'
   },
   {
     path: '/',
---
+++ north-admin/src/views/error/404.vue
@@ -0,0 +1,63 @@
+<!--
+ * @Author: north 2445951561@qq.com
+ * @Date: 2023-04-14 10:15:06
+ * @LastEditors: north 2445951561@qq.com
+ * @LastEditTime: 2023-04-14 10:48:32
+ * @FilePath: \north\north-admin\src\views\error\404.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div class="container w-full h-full bg-white">
+    <div id="svgContainer" ref="lottieRef"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+import lottie from "lottie-web";
+const props = defineProps({
+  src: {
+    type: Object,
+    default: ()=>{},
+  },
+  path: {
+    type: String,
+    default: "",
+  },
+  loop: {
+    type: Boolean,
+    default: true,
+  },
+  autoplay: {
+    type: Boolean,
+    default: true,
+  },
+});
+const lottieBox = ref(null);
+onMounted(() => {
+  if (lottieBox.value) {
+    const animation = lottie.loadAnimation({
+      container: lottieBox.value,
+      renderer: "svg", // 渲染方式:svg：支持交互、不会失帧、canvas、html：支持3D，支持交互
+      loop: props.loop, // 循环播放，默认：true
+      autoplay: props.autoplay, // 自动播放 ，默认true
+      path: props.path,//网络路径
+      animationData: Object.keys(props.src).length == 0 ? "" : props.src,//本地路径，优先级更高
+    });
+  }
+});
+</script>
+
+<style lang="scss">
+  #svgContainer {
+            width: 640px;
+            height: 512px;
+            background-color: white;
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            margin: auto;
+            }
+</style>
