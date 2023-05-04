c28d75ebeed773f5f7f258a3d7b06ed77e9eeb2e
north <2445951561@qq.com>
2023-05-04 16:33:22 +0800

    优化文件上传组件，后续需使用上传接口测试再完善

--- north-admin/src/components/FileUpload/index.vue
+++ north-admin/src/components/FileUpload/index.vue
@@ -2,7 +2,7 @@
  * @Author: north 2445951561@qq.com
  * @Date: 2023-04-26 15:33:49
  * @LastEditors: north 2445951561@qq.com
- * @LastEditTime: 2023-05-04 11:57:47
+ * @LastEditTime: 2023-05-04 16:31:03
  * @FilePath: \north\north-admin\src\components\FileUpload\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -16,10 +16,10 @@
 -->
 <template>
   <div>
-    <n-upload multiple :action="uploadAction" :file-list="myFileList" :show-file-list="false" v-model:data="fileData"
-      :disabled="disabled" :headers="headers" :max="max" @change="handleChange" @error="handleError"
-      @finish="handleFinish" @before-upload="handleUploadBefore" @remove="handleRemove">
-      <n-button>{{ btnTitle }}</n-button>
+    <n-upload multiple :action="uploadAction" :show-file-list="false" v-model:data="fileData" :disabled="isDisabled"
+      :headers="headers" :max="max" @error="handleError" @finish="handleFinish"
+      @before-upload="handleUploadBefore">
+      <n-button class="uploadButton"><n-spin v-show="isUpload" :size="12"></n-spin>{{ btnTitle }}</n-button>
     </n-upload>
     <ul v-if="myFileList && myFileList.length > 0">
       <n-scrollbar style="max-height: 120px">
@@ -31,7 +31,7 @@
           </span>
           <span class="pl-1 pr-5 border-box group-hover:text-blue-500 truncate ..." :title="item ? item : ''">{{ item
           }}</span>
-          <span class="opacity-0 text-blue-400 group-hover:opacity-100" @click="deleteFile(index)">
+          <span class="opacity-0 text-blue-400 group-hover:opacity-100" @click.stop="deleteFile(index)">
             <svg-icon icon="close" />
           </span>
         </li>
@@ -42,7 +42,7 @@
 <script lang="ts" setup>
 import type { UploadFileInfo, UploadSettledFileInfo } from 'naive-ui'
 import { GET_TOKEN } from '@/utils/auth'
-import { ref, watch } from 'vue'
+import { ref, watch, defineEmits } from 'vue'
 interface IOptions {
   file: UploadFileInfo,
   fileList: Array<UploadFileInfo>,
@@ -52,7 +52,7 @@ const props = defineProps({
   // 值
   value: [String, Array, Object],
   // 按钮标题
-  btnTitle: {
+  title: {
     type: String,
     default: '上传文件'
   },
@@ -75,20 +75,42 @@ const props = defineProps({
   max: {
     type: Number,
     default: 5
+  },
+  // 文件类型 统一小写
+  fileType: {
+    type: Array,
+    default: () => ([])
+  },
+  // 文件大小 以MB为单位
+  fileSize: {
+    type: Number,
+    default: 10
   }
 })
 const headers = ref<Object>({
   'Admin-Token': 'Bearer ' + GET_TOKEN()
 })
-let fileData = ref(props.data)
+let fileData = ref<Object>(props.data)
 let myFileList = ref<Array<string | null | undefined>>([])
+// 按钮标题
+let btnTitle = ref<string>(props.title)
+// 当前文件数量
+let fileNum = 0
+// 是否显示loading
+let isUpload = ref<boolean>(false)
+// 是否禁用按钮
+let isDisabled = ref<boolean>(false)
+const emit = defineEmits(["update:value"])
 
 watch([() => props.value, props.data], ([newVal, newData], [oldVal, oldData]) => {
   fileData.value = newData
   if (newVal) {
+    console.log('watching')
     // 首先将值转为数组
     const list = Array.isArray(newVal) ? newVal : newVal.split(',');
-    myFileList.value = myFileList.value.concat(list)
+    myFileList.value = list
+    // 修改文件数量
+    fileNum += list.length
   }
 }, { deep: true, immediate: true })
 
@@ -119,32 +141,83 @@ function isExistIcon(iconName: string): boolean {
   return isExist
 }
 
-// // 查询构建fileList
-// function getFileList(){
+// 校验上传文件格式
+function checkFileType(file: string | null | undefined): boolean {
+  if (file) {
+    const fileNameArr = file.split('.')
+    const fileExt = fileNameArr[fileNameArr.length - 1].toLowerCase()
+    // 存在文件类型限制
+    if (props.fileType.length > 0 && fileExt) {
+      return props.fileType.indexOf(fileExt) != -1
+    }
+    return true
+  }
+  return false
+}
 
-// }
+// 校验文件数量
+function checkFileNum(): boolean {
+  console.log('myFileList.value', myFileList.value)
+  fileNum = myFileList.value.length + 1
+  console.log('fileNum', fileNum)
+  return fileNum <= props.max
+}
 
-// 组件发生变化
-function handleChange(options: IOptions) {
-  console.log('组件发生变化', options)
-  // myFileList.value = options.fileList.map(item => item.fullPath)
-  console.log('myFileList', myFileList.value)
+// 判断文件大小
+function checkFileSize(size: number | undefined): boolean {
+  if (size) {
+    return size / 1024 / 1024 < props.fileSize
+  } else {
+    return false
+  }
+}
+
+// 重置上传状态
+function resetUpload() {
+  isUpload.value = false
+  btnTitle.value = props.title
+  isDisabled.value = props.disabled
 }
 
 // 文件上传之前
 function handleUploadBefore(options: IOptions) {
-  console.log('文件上传之前', options)
-  console.log('列表', options.fileList)
+  // 需要使用Promise返回，直接返回false，无法打断上传
+  return new Promise((resolve, reject) => {
+    console.log('文件上传之前', options)
+    console.log('列表', options.fileList)
+    isUpload.value = true
+    btnTitle.value = '上传中...'
+    isDisabled.value = true
+    if (!checkFileType(options.file.fullPath)) {
+      window.$message.warning('文件格式错误！')
+      resetUpload()
+      return reject(false)
+    }
+    if (!checkFileNum()) {
+      window.$message.warning('文件数量已达上限！')
+      resetUpload()
+      return reject(false)
+    }
+    if (!checkFileSize(options.file.file?.size)) {
+      window.$message.warning(`文件大小不能超过${props.fileSize}MB！`)
+      resetUpload()
+      return reject(false)
+    }
+    // 临时
+    myFileList.value.unshift(options.file.fullPath)
+    updateFileList()
+    setTimeout(() => {
+      resetUpload()
+    }, 3000)
+    return resolve(true)
+  })
 }
 
 // 文件上传结束
 function handleFinish(options: IOptions) {
   console.log('文件上传结束', options)
-}
-
-// 删除文件监听
-function handleRemove(options: IOptions) {
-  console.log('删除文件', options)
+  resetUpload()
+  updateFileList()
 }
 
 // 预览文件
@@ -155,19 +228,31 @@ function handlePreview(url: string | null | undefined) {
     return
   }
   window.open(url, 'newwindow')
-
 }
 
 // 删除文件
 function deleteFile(index: number) {
-  console.log('deleteFile', index)
-  console.log('myFileList', myFileList.value)
   myFileList.value.splice(index, 1)
-  console.log('after', myFileList.value)
+  fileNum = myFileList.value.length
+  updateFileList()
 }
 
 // 文件上传失败
 function handleError(options: IOptions) {
   console.log('文件上传失败', options)
+  resetUpload()
+  updateFileList()
+}
+
+// 更新文件列表
+function updateFileList() {
+  emit("update:value", myFileList.value.length > 0 ? myFileList.value.join(',') : null)
 }
 </script>
+<style lang="scss" scoped>
+.uploadButton {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- north-admin/src/views/components/index.vue
+++ north-admin/src/views/components/index.vue
@@ -2,7 +2,7 @@
  * @Author: north 2445951561@qq.com
  * @Date: 2023-04-27 10:26:58
  * @LastEditors: north 2445951561@qq.com
- * @LastEditTime: 2023-04-28 16:25:23
+ * @LastEditTime: 2023-05-04 16:08:41
  * @FilePath: \north\north-admin\src\views\components\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -15,10 +15,11 @@
           <span class="w-full inline-block font-bold italic py-4">图片上传组件</span>
         </div>
         <div class="flex flex-col justify-start item-center flex-1">
-          <FileUpload :data="fileData" v-model:value="url"></FileUpload>
+          <FileUpload v-model:value="url" :fileType="['pdf']" :fileSize="1"></FileUpload>
           <span class="w-full inline-block font-bold italic py-4">文件上传组件</span>
         </div>
         <n-button @click="updateData">更新数据{{ fileData }}</n-button>
+        <n-button @click="getUrl">查看数据</n-button>
       </div>
     </n-card>
   </div>
@@ -30,9 +31,12 @@ import { reactive, ref } from 'vue'
 let fileData = reactive<any>({ name: null, type: null })
 let url = ref<any>(null)
 function updateData() {
-  fileData.name = '数据'
-  fileData.type = 'file'
+  // fileData.name = '数据'
+  // fileData.type = 'file'
   url.value = 'https://pic.616pic.com/ys_b_img/00/06/20/novPzrqvE3.jpg,https://dogefs.s3.ladydaily.com/~/source/unsplash/photo-1682685797208-c741d58c2eff,https://dogefs.s3.ladydaily.com/~/source/unsplash/photo-1681325655883-14885bd9cd93'
   console.log(url.value)
+}
+function getUrl(){
+  console.log('getUrl', url.value)
 }
 </script>
